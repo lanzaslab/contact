7ce8201ef69a29f09a11733ae4b1be5f5f66452a
tsfarthi <tsfarthi@ncsu.edu>
2020-02-13 23:23:42 -0500

    fixed a bug that required all days in the data set to be sequential when differentiating block units.

--- R/contactTest.R
+++ R/contactTest.R
@@ -455,6 +455,7 @@ contactTest<-function(emp.input, rand.input, dist.input = NULL, test = "chisq", 
   }
   
   timeBlock.append<-function(x, dateTime = NULL, blockLength, blockUnit){
+    
     datetime.append1 = function(x){
       
       timevec = x$dateTime
@@ -463,11 +464,14 @@ contactTest<-function(emp.input, rand.input, dist.input = NULL, test = "chisq", 
       dateseq = unique(lub.dates)
       dayIDList = NULL
       dayIDseq = seq(1,(length(dateseq)),1)
+      
+      dayDiffSeq<-NULL
+      
       for(b in dayIDseq){
-        ID = rep(b,length(which(lub.dates == dateseq[which(dayIDseq == b)])))
-        dayIDList = c(dayIDList, ID)
+        dayDiff<- rep(difftime(timevec[b] ,timevec[1] , units = c("days")), length(which(lub.dates == dateseq[which(dayIDseq == b)])))
+        dayDiffSeq<-c(dayDiffSeq, dayDiff)
       } 
-      x$totalSecond = ((dayIDList - min(dayIDList))*86400) + daySecondList #This calculates the total second (the cumulative second across the span of the study's timeframe)
+      x$totalSecond = (dayDiffSeq*86400) + daySecondList #This calculates the total second (the cumulative second across the span of the study's timeframe)
       
       return(x)
     }
@@ -488,6 +492,17 @@ contactTest<-function(emp.input, rand.input, dist.input = NULL, test = "chisq", 
       blockLength1 <- blockLength*60*60*24*7 #num seconds in a week
     }
     
+    if(length(x) == 0){ #if there is no x input (i.e., x == NULL), #assumes that if x == NULL, dateTime does not.
+      x<- data.frame(dateTime = dateTime)
+    }else{ # length(x) > 0
+      if(length(dateTime) > 0){ #dateTime == NULL, the function assumes that there is a "dateTime" column in x.
+        if(length(dateTime) == 1 && is.na(match(dateTime[1], names(x))) == FALSE){ #added 1/14 to accompany the list-processing functionality. If x is a list, rather than point.x being a vector of length(nrow(x)), it may be necessary to designate the colname for intended "point.x" values (i.e., if the x-coordinate values in different list entries are different)
+          x$dateTime <- x[,match(dateTime, names(x))]
+        }else{ #if length(dateTime) > 1
+          x$dateTime = dateTime
+        }
+      }
+    }
     x<-x[order(x$dateTime),] #Just in case the data wasn't already ordered in this way.
     x<-datetime.append1(x) #adds the total second column to the dataframe
     studySecond <- (x$totalSecond -min(x$totalSecond)) + 1
@@ -500,9 +515,23 @@ contactTest<-function(emp.input, rand.input, dist.input = NULL, test = "chisq", 
     if(length(which(block == 0)) > 0){ #identifies the last block
       block[which(block == 0)] = numblocks
     }
-    x$block = block
+    blockVec<-unique(block)
+    dateTimeVec2<-x$dateTime #dateTime after x is sorted
+    minBlockTimeSeq <- rep(0, length(block)) #Added 2/4/2019. This vector will identify the minimum timepoint in each block.
+    maxBlockTimeSeq <- rep(0, length(block)) #Added 2/4/2019. This vector will identify the maximum timepoint in each block.
+    for(f in 1:length(blockVec)){
+      minBlockTime<-as.character(dateTimeVec2[min(which(block == blockVec[f]))])
+      minBlockTimeSeq[which(block == blockVec[f])] <- minBlockTime
+      maxBlockTime<-as.character(dateTimeVec2[max(which(block == blockVec[f]))])
+      maxBlockTimeSeq[which(block == blockVec[f])] <- maxBlockTime
+    }
+    x$block <- block
+    x$block.start <- minBlockTimeSeq
+    x$block.end <- maxBlockTimeSeq
+    x$numBlocks <- max(blockVec) #the contactTest function will require thus information (i.e. the number of blocks in the dataset)
     return(x)
   }
+  
 
   chisq.forLoop<-function(x, empirical = x, randomized = y, dist = dist.input, x.blocking, y.blocking, listStatus.x){ #I hate that I have to do this in a for-loop, but I couldn't get the apply functions to work.
     
