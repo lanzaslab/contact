25e9623fa9d2c98d002f0a705963f49880341249
Trevor <tsfarthi@ncsu.edu>
2020-03-20 00:01:26 -0400

    updated documentation

--- R/contactTest.R
+++ R/contactTest.R
@@ -10,11 +10,13 @@
 #' 
 #' @param ... Any input will return the error message: "'contactTest' is now 
 #'    defunct. Please consider using another contact-comparison function 
-#'    instead (e.g., contactCompare_chisq, contactCompare_mantel, etc.)."
+#'    instead (e.g., \code{\link{contactCompare_chisq}}, 
+#'    \code{\link{contactCompare_mantel}}, etc.)."
 #' @keywords network-analysis social-network defunct
 #' @return Always returns the error message: "'contactTest' is now 
 #'    defunct. Please consider using another contact-comparison function 
-#'    instead (e.g., contactCompare_chisq, contactCompare_mantel, etc.)."
+#'    instead (e.g., \code{\link{contactCompare_chisq}}, 
+#'    \code{\link{contactCompare_mantel}}, etc.)."
 #'    
 #' @export
 
